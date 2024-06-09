d5bc36418708c9ca59481973933ec6f9a284895f
sonnylau98 <129824979+sonnylau98@users.noreply.github.com>
2024-06-09 23:07:46 +0800

    Delete target directory

--- target/debug/deps/rand_chacha-2273ef15371d1880.d
+++
@@ -1,9 +0,0 @@
-/home/vb/Desktop/guessing_game/target/debug/deps/librand_chacha-2273ef15371d1880.rmeta: /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vb/Desktop/guessing_game/target/debug/deps/librand_chacha-2273ef15371d1880.rlib: /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vb/Desktop/guessing_game/target/debug/deps/rand_chacha-2273ef15371d1880.d: /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
-/home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
-/home/vb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
--- target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/vb/Desktop/guessing_game/target/debug/guessing_game: /home/vb/Desktop/guessing_game/src/main.rs
